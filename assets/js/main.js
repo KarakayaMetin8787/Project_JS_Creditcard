f239859a83d93a5eb5e65be4dc834f78d1b4c5a2
KarakayaMetin8787 <karakayametin8787@gmail.com>
2023-10-23 09:29:46 +0200

    cardnumber formatiert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 function transfer(){
-    const cardNumberValue = document.body.querySelector("#cardNumber");
+    const cardNumberValue = document.body.querySelector("#cardNumber").value;
     const cardNameValue = document.body.querySelector("#cardHolder");
     const cardDate = document.body.querySelector("#expDate");
     const cardCvv = document.body.querySelector("#cvv");
+    
+    const cardNumberFormatted = cardNumberValue.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1 $2 $3 $4');
 
-    document.body.querySelector(".cardContainer > p").textContent = Number(cardNumberValue.value);
+    document.body.querySelector(".cardContainer > p").textContent = cardNumberFormatted;
     document.body.querySelector(".cardContainer div span:nth-of-type(1)").textContent = cardNameValue.value;
     document.body.querySelector(".cardContainer div span:nth-of-type(2)").textContent = cardDate.value;
     document.body.querySelector(".cvv").textContent = cardCvv.value;
